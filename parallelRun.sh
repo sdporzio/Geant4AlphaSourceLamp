4d74a6ab6270f0a59a84e0ecf948427418f44edd
Davide Porzio <porziodavide@gmail.com>
2020-07-02 07:05:34 -0700

    update

--- parallelRun.sh
+++ parallelRun.sh
@@ -1,11 +1,5 @@
-screen -dmS 1.2mm bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 0 1.2"
+screen -dmS mode2 bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 2 1"
 sleep 1
 
-screen -dmS 1.4mm bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 0 1.4"
-sleep 1
-
-screen -dmS 1.6mm bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 0 1.6"
-sleep 1
-
-screen -dmS 1.8mm bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 0 1.8"
+screen -dmS mode3 bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 3 1"
 sleep 1
