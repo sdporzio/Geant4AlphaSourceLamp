a041377dd0b12a16ada666054e6daaa365e7abfd
Davide S. Porzio <davideporziohep@gmail.com>
2020-09-24 15:55:29 +0100

    fixing alias

--- 3_runOnly.sh
+++ 3_runOnly.sh
@@ -3,7 +3,7 @@ export MG4_RANDOMSEED=100
 export MG4_VISUAL=0
 export MG4_HOLETYPE=3
 export MG4_HOLEWIDTH=1
-export MG4_HOLEOFFSET={$1}
+export MG4_HOLEOFFSET=${1}
 
 export MG4_OUTDIR="/global/project/projectdirs/lz/users/sdporzio/Data/AlphaSourceVUVLamp"
 export MG4_FILENAME="${MG4_HOLEOFFSET}-off_${MG4_NEVENTS}-ev_${MG4_RANDOMSEED}-s.root"
