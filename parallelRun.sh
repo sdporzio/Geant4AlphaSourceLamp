4bb5b88c2f40c69ad70f1254fe1d5270881bf19e
Davide S. Porzio <davideporziohep@gmail.com>
2020-09-28 06:51:06 +0100

    better naming of files and n_ev up to 50M

--- parallelRun.sh
+++ parallelRun.sh
@@ -1,6 +1,6 @@
 /global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/2_compileOnly.sh
 
-screen -dmS offset_0 bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 0."
+screen -dmS offset_0 bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 0.0"
 sleep 1
 
 screen -dmS offset_0.5 bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 0.5"
@@ -9,5 +9,5 @@ sleep 1
 screen -dmS offset_0.75 bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 0.75"
 sleep 1
 
-screen -dmS offset_1 bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 1."
+screen -dmS offset_1 bash -c "/global/project/projectdirs/lz/users/sdporzio/AlphaSourceLamp/3_runOnly.sh 1.0"
 sleep 1
